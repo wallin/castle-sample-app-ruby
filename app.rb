f58a4cd18e77b798277115390a101eedf3e7ba5d
wallin <sebastian.wallin@gmail.com>
2023-04-27 16:54:27 +0200

    Add sidebar for request log

--- app.rb
+++ app.rb
@@ -2,10 +2,17 @@ require 'base64'
 require 'castle'
 require 'sinatra'
 require 'sinatra/cookies'
+require 'faye/websocket'
+require 'time'
+require 'digest'
 
-# Configure and initialize the Castle client
-Castle.configure do |config|
-  config.api_secret = ENV['CASTLE_API_SECRET']
+Faye::WebSocket.load_adapter('puma')
+
+$connections = []
+
+configure do
+  enable :sessions
+  set :session_secret, ENV.fetch('SESSION_SECRET', 'default_secret')
 end
 
 def user_data(request)
@@ -26,7 +33,7 @@ get '/' do
   castle_response = request.params['status']
   if castle_response
     @message = 'Login Failed. Hint: try "password" as password'
-  elsif ENV['CASTLE_API_SECRET'].nil? || ENV['CASTLE_PUB_KEY'].nil?
+  elsif (session['api_key'] || ENV['CASTLE_API_SECRET']).nil? || (session['pub_key'] || ENV['CASTLE_PUB_KEY']).nil?
     @message = 'Please set CASTLE_API_SECRET and CASTLE_PUB_KEY in your ENV'
   end
 
@@ -103,6 +110,69 @@ post '/signup' do
   redirect '/overview'
 end
 
+############################## Admin related
+
+get '/admin' do
+  erb :ws
+end
+
+get '/ws' do
+  if Faye::WebSocket.websocket?(request.env)
+    ws = Faye::WebSocket.new(request.env)
+
+    ws.on :open do |_event|
+      $connections << ws
+    end
+
+    ws.on :message do |event|
+      $connections.each { |conn| conn.send(event.data) }
+    end
+
+    ws.on :close do |_event|
+      $connections.delete(ws)
+    end
+
+    ws.rack_response
+  else
+    status 400
+  end
+end
+
+post "/configure" do
+  api_key = params["api-key"]
+  pub_key = params["pub-key"]
+
+  session[:api_key] = api_key
+  session[:pub_key] = pub_key
+
+  status 200
+  redirect "/admin"
+end
+
+delete '/configure' do
+  session[:api_key] = nil
+  session[:pub_key] = nil
+
+  status 200
+  redirect '/admin'
+end
+
+post '/messages' do
+  params = castle_params(request).merge(
+    event: '$custom',
+    name: 'message_sent',
+    user: { id: cookies[:user_id], email: cookies[:user_email] },
+    properties: {
+      message_hash: Digest::MD5.hexdigest(request.params['message'])
+    }
+  )
+
+  castle_response = send_to_castle('risk', params)
+
+  status 200
+  redirect '/overview'
+end
+
 private
 
 # Helper methods for sending requests to Castle
@@ -119,7 +189,19 @@ def castle_params(request)
 end
 
 def send_to_castle(endpoint, params)
-  $castle.public_send(endpoint, params)
-rescue Castle::InvalidParametersError => e
-  e.message
+  # Configure and initialize the Castle client
+  Castle.configure do |config|
+    config.api_secret = session[:api_key] || ENV['CASTLE_API_SECRET']
+  end
+
+  resp = $castle.public_send(endpoint, params)
+  message = {
+    event: params[:event],
+    status: params[:status] || params[:name],
+    timestamp: Time.now.utc.iso8601.to_s,
+    response: resp.to_h
+  }
+  $connections.each { |conn| conn.send(JSON.dump(message)) }
+# rescue => e
+#  $connections.each { |conn| conn.send(e.message) }
 end
